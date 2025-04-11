2613d2b664dd5621524fd69d4dba3a85448a2e90
cswimr <seaswimmerthefsh@gmail.com>
2025-04-11 13:19:14 -0500

    improve nix flake

--- flake.nix
+++ flake.nix
@@ -1,48 +1,32 @@
 {
-  description = "A Nix-flake-based Node.js development environment";
-
-  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    devshell.url = "github:numtide/devshell";
+    corepack.url = "github:SnO2WMaN/corepack-flake";
+  };
 
   outputs =
-    { self, nixpkgs }:
-    let
-      supportedSystems = [
-        "x86_64-linux"
-        "aarch64-linux"
-        "x86_64-darwin"
-        "aarch64-darwin"
-      ];
-      forEachSupportedSystem =
-        f:
-        nixpkgs.lib.genAttrs supportedSystems (
-          system:
-          f {
-            pkgs = import nixpkgs {
-              inherit system;
-              overlays = [ self.overlays.default ];
-            };
-          }
-        );
-    in
     {
-      overlays.default = final: prev: rec {
-        nodejs = prev.nodejs;
-      };
-
-      devShells = forEachSupportedSystem (
-        { pkgs }:
-        {
-          default = pkgs.mkShell {
-            packages = with pkgs; [
-              node2nix
-              nodejs
-              nodePackages.pnpm
-            ];
-            shellHook = ''
-              pnpm i --frozen
-            '';
-          };
-        }
-      );
-    };
+      self,
+      nixpkgs,
+      flake-utils,
+      ...
+    }@inputs:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = with inputs; [
+            devshell.overlays.default
+            corepack.overlays.default
+          ];
+        };
+      in
+      {
+        devShells.default = pkgs.devshell.mkShell {
+          imports = [ (pkgs.devshell.importTOML ./devshell.toml) ];
+        };
+      }
+    );
 }
