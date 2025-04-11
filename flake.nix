43a3095675c8cdc25b0241909e16cb7646301c37
cswimr <seaswimmerthefsh@gmail.com>
2025-04-11 16:56:40 -0500

    switch back to configuring the flake inside of the flake

--- flake.nix
+++ flake.nix
@@ -2,7 +2,6 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     devshell.url = "github:numtide/devshell";
-    corepack.url = "github:SnO2WMaN/corepack-flake";
   };
 
   outputs =
@@ -19,13 +18,47 @@
           inherit system;
           overlays = with inputs; [
             devshell.overlays.default
-            corepack.overlays.default
           ];
         };
+        # Read and parse package.json
+        pkg = builtins.fromJSON (builtins.readFile ./package.json);
+
+        # Create a function that formats a command using the key and its value.
+        makeCommand = key: {
+          name = "pnpm " + key;
+          command = ''
+            #!/usr/bin/env bash
+            pnpm ${key}
+          '';
+          help = pkg.scripts.${key};
+          category = "scripts";
+        };
+
+        # Get all the script keys
+        scriptNames = builtins.attrNames pkg.scripts;
+
+        # Map over the keys to generate the list of command attributes.
+        commandsFromScripts = builtins.map makeCommand scriptNames;
       in
       {
         devShells.default = pkgs.devshell.mkShell {
-          imports = [ (pkgs.devshell.importTOML ./devshell.toml) ];
+          commands =
+            with pkgs;
+            [
+              {
+                package = nodejs_23;
+                name = "node";
+              }
+              {
+                package = pnpm;
+              }
+            ]
+            ++ commandsFromScripts;
+
+          motd = ''
+            {73}🔨 Welcome to the {201}Astro{105}pedia{73} Devshell!{reset}
+            $(type -p menu &>/dev/null && menu)
+          '';
         };
       }
     );
